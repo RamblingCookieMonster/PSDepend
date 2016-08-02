0052a6bbff2b2cc7bf8aac1f53c6da6323c4f33e
RamblingCookieMonster <RamblingCookieMonster@users.noreply.github.com>
2016-08-01 21:17:40 -0400

    github client is slow.

--- PSDepend/PSDependScripts/Noop.ps1
+++ PSDepend/PSDependScripts/Noop.ps1
@@ -1,34 +1,35 @@
 <#
     .SYNOPSIS
-        Display variables that a deployment script would receive.
+        Display variables that a dependency script would receive.
 
         Used for testing and validation.
 
     .DESCRIPTION
-        Display variables that a deployment script would receive.
+        Display variables that a dependency script would receive.
 
         Used for testing and validation.
 
-    .PARAMETER Deployment
-        Deployment to process
+    .PARAMETER Dependency
+        Dependency to process
 
     .PARAMETER StringParameter
         An example parameter that does nothing
 #>
 [cmdletbinding()]
 param (
-    [ValidateScript({ $_.PSObject.TypeNames[0] -eq 'PSDeploy.Deployment' })]
-    [psobject[]]$Deployment,
+    [PSTypeName('PSDepend.Dependency')]
+    [psobject[]]
+    $Dependency,
 
     [string[]]$StringParameter
 )
 
-Write-Verbose "Starting noop run with $($Deployment.count) sources"
+Write-Verbose "Starting noop run with $($Dependency.count) sources"
 
 [pscustomobject]@{
     PSBoundParameters = $PSBoundParameters
-    Deployment = $Deployment
-    DeploymentOptions = $Deployment.DeploymentOptions
+    Dependency= $Dependency
+    DependencyParameters = $Dependency.Parameters
     GetVariable = (Get-Variable)
     ENV = Get-Childitem ENV:
 }
