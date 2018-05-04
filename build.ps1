eeec50f3189071e9d8bc42fec845c1f45e8aff44
Oliver Lipkau <oliver@lipkau.net>
2018-05-04 06:25:12 +0200

    Added error handling parameters
    allows build to run locally without throwing errors if user is not admin

--- build.ps1
+++ build.ps1
@@ -3,11 +3,11 @@
 # Grab nuget bits, install modules, set build variables, start build.
 Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
 
-Install-Module Psake, PSDeploy, BuildHelpers -force -AllowClobber
-Install-Module Pester -MinimumVersion 4.1 -Force -AllowClobber -SkipPublisherCheck
+Install-Module Psake, PSDeploy, BuildHelpers -force -AllowClobber -Scope CurrentUser
+Install-Module Pester -MinimumVersion 4.1 -Force -AllowClobber -SkipPublisherCheck -Scope CurrentUser
 Import-Module Psake, BuildHelpers
 
-Set-BuildEnvironment
+Set-BuildEnvironment -ErrorAction SilentlyContinue
 
 Invoke-psake -buildFile $ENV:BHProjectPath\psake.ps1 -taskList $Task -nologo
 exit ( [int]( -not $psake.build_success ) )
