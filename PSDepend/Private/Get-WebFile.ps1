87005ca383170da577896cb5a33bd2c38a1c78a8
Raimund Andée [MSFT] <raandree@live.com>
2018-05-30 12:21:26 +0200

    Support for TLS1.2

--- PSDepend/Private/Get-WebFile.ps1
+++ PSDepend/Private/Get-WebFile.ps1
@@ -2,6 +2,7 @@
 Function Get-WebFile {
     param($URL, $Path)
     # We have the info, check for file, download it!
+	[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
     $webclient = New-Object System.Net.WebClient
     $webclient.DownloadFile($URL, $Path)
     $webclient.Dispose()
